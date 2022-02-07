9858f0a38ba67853bb98349955b5eab48b470550
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 13:46:23 +0530

    Added: odd Even Problem

---
+++ src/javapractice/FlipCoin.java
@@ -0,0 +1,21 @@
+package javapractice;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlipCoin {
+	public static void flipcoin() {
+		System.out.println("Enter the number of times to flip a coin");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		Random random = new Random();
+		if (num > 0) {
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {	
+
+	}
+
+}
---
+++ src/javapractice/OddEven.java
@@ -0,0 +1,26 @@
+package javapractice;
+
+import java.util.Scanner;
+
+public class OddEven {
+	
+		static void checkEvenorOdd(int num) {
+			// If remainder is zero then this number is even
+			if (num % 2 == 0) {
+				System.out.println("Number is even");
+			}
+			else {
+			// If remainder is not zero then this number is odd
+				System.out.println("number is odd");
+			}	
+		}
+
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);  
+			System.out.println("Enter any number");
+			int num = sc.nextInt();
+			checkEvenorOdd(num);
+			sc.close();
+	}
+}
