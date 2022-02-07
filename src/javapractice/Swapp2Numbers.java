b8c1299e4b339cea15b95bc29b973fec0e4b0a23
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 15:35:41 +0530

    Added: Swapping of two numbers

--- src/javapractice/Swapp2Numbers.java
+++ src/javapractice/Swapp2Numbers.java
@@ -8,12 +8,12 @@ public class Swapp2Numbers {
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
-		System.out.println("before swapping numbers: "+num1 +" "+ num2);  
-		num1 = num1 + num2;   
-        num2 = num1 - num2;   
-        num1 = num1 - num2;  
-        System.out.println("After swapping: "+num1 +"  " + num2);   
-        sc.close();
+		System.out.println("before swapping numbers: " + num1 + " " + num2);
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		System.out.println("After swapping: " + num1 + "  " + num2);
+		sc.close();
 	}
 
 	public static void main(String[] args) {
